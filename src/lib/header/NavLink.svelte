a2d7ed76a806a5ff1803df42afce41529caff697
Julien <jzapata@live.nl>
2021-11-07 13:58:10 +0100

    feat: add header

---
+++ src/lib/header/NavLink.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	export let href = '';
+	export let title = '';
+	export let text = '';
+	export let className = '';
+</script>
+
+<a {href} class={`${className} link-animated-hover first:ml-0 last:mr-0 mx-2`} title={title ?? text}>
+	{text}
+</a>
