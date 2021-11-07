77a655639bcf9767a7c07b854245fbbd64ab13c1
Julien <jzapata@live.nl>
2021-11-07 17:08:01 +0100

    feat: add fallback 404 page

---
+++ src/routes/[404]/index.svelte
@@ -0,0 +1,7 @@
+<div class="flex flex-col justify-center text-center h-screen">
+	<div>
+		<h1>This page does not exist</h1>
+
+		<a href="/" class="link link-animated-hover">Back to the homepage</a>
+	</div>
+</div>
--- svelte.config.js
+++ svelte.config.js
@@ -23,7 +23,7 @@ const config = {
 			// default options are shown
 			pages: 'build',
 			assets: 'build',
-			fallback: null
+			fallback: '404.html'
 		})
 	}
 };
